634d59085a34d5a13a806017955ba4fcec606535
Alejandro Gómez <69641298+AlejoGomezC@users.noreply.github.com>
2025-04-02 16:05:45 -0500

    Update generate_docs.sh

--- generate_docs.sh
+++ generate_docs.sh
@@ -14,7 +14,7 @@ if [ ! -s Club.postman_collection.json ]; then
 fi
 echo "JSON file exists and is not empty"
 
-jq -r '.item[] | "- **" + .name + "**\n  - Method: " + .request.method + "\n  - URL: " + .request.url.raw + "\n  - Body: ```json\n" + (.request.body.raw // "No body") + "\n```\n  - Response: ```json\n" + (.response[0].body // "No response") + "\n```\n"' Club.postman_collection.json | sed 's/\\n/\n/g' >> docs/README.md
+jq -r '.item[] | "- **" + .name + "**\n  - Method: " + .request.method + "\n  - URL: " + .request.url.raw + "\n  - Body:\n\n```json\n" + (.request.body.raw // "No body") + "\n```\n  - Response:\n\n```json\n" + (.response[0].body // "No response") + "\n```\n"' Club.postman_collection.json >> docs/README.md
 echo "Parsed JSON and updated README.md"
 
 newman run Club.postman_collection.json -r html --reporter-html-export docs/postman_report.html
